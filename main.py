544ab53ef09719f885cfe6291fa2cb7dad53ba29
JuliaWieczorek <wieczorek.julia.76@gmail.com>
2023-02-01 20:26:08 +0100

    deep learning, weights and plot

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import numpy as np
+from keras.models import Model
+from tensorflow.keras.layers import Input
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+
 import preprocessing
 # Defining the window for context
 window = 2
@@ -62,5 +67,42 @@ for i, word_list in enumerate(word_lists):
 # Converting the matrices into an array
 X = np.asarray(X)
 Y = np.asarray(Y)
-print(X)
-print(Y)
+
+# Deep learning
+
+# The size of the embedding = the hidden layer dimension
+embed_size = 2
+
+# Defining the size of the embedding
+inp = Input(shape=(X.shape[1],))
+x = Dense(units=embed_size, activation='linear')(inp)
+x = Dense(units=Y.shape[1], activation='softmax')(x)
+model = Model(inputs=inp, outputs=x)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# Optimizing the network weights
+model.fit(
+    x=X,
+    y=Y,
+    batch_size=256,
+    epochs=1000
+    )
+# Obtaining the weights from the neural network.
+# These are the so called word embeddings
+
+# The input layer
+weights = model.get_weights()[0]
+
+# Creating a dictionary to store the embeddings in. The key is a unique word and the value is the numeric vector
+embedding_dict = {}
+for word in words:
+    embedding_dict.update({word: weights[unique_word_dict.get(word)]})
+
+print(embedding_dict)
+plt.figure(figsize=(10, 10))
+for word in list(unique_word_dict.keys()):
+    coord = embedding_dict.get(word)
+    plt.scatter(coord[0], coord[1])
+    plt.annotate(word, (coord[0], coord[1]))
+plt.show()
+plt.savefig('Visualization of the embeddings.png')
