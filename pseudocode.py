0aacfc922429d2049b13c3bfa3260135e1f85b9e
JuliaWieczorek <wieczorek.julia.76@gmail.com>
2023-02-01 20:32:57 +0100

    delete files

--- pseudocode.py
+++
@@ -1,9 +0,0 @@
-'''
-PSEUDOCODE
-1. Read the text
-2. Preprocess text
-3. Create (x, y) data points
-4. Create one hot encoded (X, Y) matrices
-5. Train a neural network
-8. Extract the weights from the input layer
-'''
