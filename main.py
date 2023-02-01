8b57139d9ca52313cf221a8eaa6f337a5b90ea6e
JuliaWieczorek <wieczorek.julia.76@gmail.com>
2023-02-01 20:02:15 +0100

    sparse vectors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import preprocessing
 # Defining the window for context
 window = 2
@@ -22,4 +23,44 @@ for sentence in all_text:
             if i-1-next >= 0:
                 word_lists.append((word, sentence[i-1-next]))
 
-print(word_lists)
+def unique_word(text:list):
+    list_text = []
+    for line in text:
+        for word in line.split():
+            list_text.append(word)
+    words = list(set(list_text))
+    words.sort()
+    unique_word = {}
+    for i, word in enumerate(words):
+        unique_word.update({word: i})
+    return unique_word
+
+# number of features
+unique_word_dict = unique_word(all_text)
+n_words = len(unique_word_dict)
+words = list(unique_word_dict.keys())
+
+# Creating the X and Y matrices using one hot encoding
+X = []
+Y = []
+for i, word_list in enumerate(word_lists):
+    main_word_index = unique_word_dict.get(word_list[0])
+    context_word_index = unique_word_dict.get(word_list[1])
+
+    # Creating the placeholders
+    X_row = np.zeros(n_words)
+    Y_row = np.zeros(n_words)
+
+    # One hot encoding the main and the context word
+    X_row[main_word_index] = 1
+    Y_row[context_word_index] = 1
+
+    # Main matrices
+    X.append(X_row)
+    Y.append(Y_row)
+
+# Converting the matrices into an array
+X = np.asarray(X)
+Y = np.asarray(Y)
+print(X)
+print(Y)
